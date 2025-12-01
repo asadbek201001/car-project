ddd1841fd4d7f43d3879e13da96936118c8a7672
Ismingiz <email@example.com>
2025-12-01 16:52:34 +0500

    code pusshign with contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,12 +22,36 @@ const InfoSection = styled.div`
   margin-top: -350px;
   margin-bottom: 80px;
   padding: 40px 20px;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    margin-top: -200px;
+    margin-bottom: 40px;
+    padding: 20px 10px;
+    gap: 30px;
+  }
+
+  @media (max-width: 600px) {
+    margin-top: -100px;
+    padding: 15px 5px;
+    gap: 20px;
+  }
 `;
 
 const InfoItem = styled.div`
   display: flex;
   align-items: center;
   gap: 15px;
+
+  @media (max-width: 900px) {
+    width: 80%;
+    justify-content: flex-start;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    gap: 10px;
+  }
 `;
 
 const IconCircle = styled.div`
@@ -38,6 +62,11 @@ const IconCircle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 600px) {
+    width: 40px;
+    height: 40px;
+  }
 `;
 
 const TextBlock = styled.div`
@@ -48,11 +77,19 @@ const TextBlock = styled.div`
 const ItemTitle = styled.div`
   font-size: 15px;
   color: #444;
+
+  @media (max-width: 600px) {
+    font-size: 13px;
+  }
 `;
 
 const ItemValue = styled.div`
   font-size: 16px;
   font-weight: 600;
+
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
 `;
 
 const ContactContainer = styled.div`
@@ -63,6 +100,15 @@ const ContactContainer = styled.div`
   justify-content: center;
   font-size: 24px;
   font-family: Arial, Helvetica, sans-serif;
+
+  @media (max-width: 900px) {
+    height: auto;
+    padding: 40px 20px;
+  }
+
+  @media (max-width: 600px) {
+    padding: 30px 10px;
+  }
 `;
 
 const ContactContainer2 = styled.div`
@@ -74,6 +120,18 @@ const ContactContainer2 = styled.div`
   font-size: 24px;
   margin-top: 50px;
   font-family: Arial, Helvetica, sans-serif;
+
+  @media (max-width: 900px) {
+    font-size: 20px;
+    margin-top: 30px;
+    padding: 0 20px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 18px;
+    margin-top: 20px;
+    padding: 0 10px;
+  }
 `;
 
 const MainText = styled.h1`
@@ -83,6 +141,20 @@ const MainText = styled.h1`
   font-weight: bold;
   margin-bottom: 40px;
   font-family: 'Work Sans', sans-serif;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    height: auto;
+    font-size: 28px;
+    margin-top: 40px;
+    margin-bottom: 30px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 24px;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
 `;
 
 const MainText2 = styled.h1`
@@ -93,6 +165,17 @@ const MainText2 = styled.h1`
   justify-content: center;
   font-weight: bold;
   font-family: 'Work Sans', sans-serif;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    font-size: 24px;
+    padding: 0 20px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 20px;
+    padding: 0 10px;
+  }
 `;
 
 const SmallText = styled.p`
@@ -105,6 +188,18 @@ const SmallText = styled.p`
   display: flex;
   font-family: Arial, Helvetica, sans-serif;
   margin-bottom: 40px;
+
+  @media (max-width: 900px) {
+    width: auto;
+    margin-left: 0;
+    font-size: 16px;
+    margin-bottom: 30px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 14px;
+    margin-bottom: 20px;
+  }
 `;
 
 const MainContainer = styled.div`
@@ -113,6 +208,16 @@ const MainContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    height: auto;
+    padding: 20px 10px;
+  }
+
+  @media (max-width: 600px) {
+    padding: 15px 5px;
+  }
 `;
 
 const MainContainer2 = styled.div`
@@ -124,6 +229,20 @@ const MainContainer2 = styled.div`
   gap: 30px;
   padding-top:60px;
   padding-bottom:60px;  
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    height: auto;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    gap: 20px;
+  }
+
+  @media (max-width: 600px) {
+    padding-top: 30px;
+    padding-bottom: 30px;
+    gap: 15px;
+  }
 `;
 
 const MainContainer3 = styled.div`
@@ -141,6 +260,22 @@ const MainContainer3 = styled.div`
   gap: 130px;
   margin-left:98px;
   margin-top:75px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    flex-wrap: wrap;
+    height: auto;
+    margin-left: 0;
+    padding: 40px 20px;
+    gap: 50px;
+    justify-content: center;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    padding: 30px 10px;
+    gap: 30px;
+  }
 `;
 
 const CarsLogoSection = styled.div`
@@ -165,6 +300,20 @@ const CarCard3 = styled.div`
   margin-right: 50px;
   margin-bottom: 500px;
   gap: 30px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+    margin-right: 0;
+    margin-bottom: 40px;
+    padding: 30px 20px;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    margin-bottom: 30px;
+    padding: 20px 15px;
+  }
 `;
 
 const SelectBox = styled.select`
@@ -180,6 +329,16 @@ const SelectBox = styled.select`
   background-image: url("data:image/svg+xml;utf8,<svg width='16' height='16' xmlns='http://www.w3.org/2000/svg'><path d='M4 6l4 4 4-4' stroke='black' fill='none' stroke-width='2' stroke-linecap='round'/></svg>");
   background-repeat: no-repeat;
   background-position: right 15px center;
+
+  @media (max-width: 900px) {
+    height: 40px;
+    font-size: 14px;
+  }
+
+  @media (max-width: 600px) {
+    height: 35px;
+    font-size: 13px;
+  }
 `;
 
 const DateBox = styled.select`
@@ -196,6 +355,16 @@ const DateBox = styled.select`
   background-repeat: no-repeat;
   background-position: right 15px center;
   background-size: 16px 16px;
+
+  @media (max-width: 900px) {
+    height: 40px;
+    font-size: 14px;
+  }
+
+  @media (max-width: 600px) {
+    height: 35px;
+    font-size: 13px;
+  }
 `;
 
 const InputCtn = styled.div`
@@ -234,6 +403,33 @@ const InputCtn = styled.div`
       cursor: pointer;
     }
   }
+
+  @media (max-width: 900px) {
+    width: 100%;
+    height: auto;
+
+    input {
+      height: 40px;
+    }
+
+    button {
+      height: 40px;
+      font-size: 16px;
+    }
+  }
+
+  @media (max-width: 600px) {
+    gap: 10px;
+
+    input {
+      height: 35px;
+    }
+
+    button {
+      height: 35px;
+      font-size: 14px;
+    }
+  }
 `;
 
 const CarImageBlur = styled.img`
@@ -242,6 +438,17 @@ const CarImageBlur = styled.img`
   background-size: cover;
   border-radius: 20px;
   margin-bottom: 500px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+    margin-bottom: 40px;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    margin-bottom: 30px;
+  }
 `;
 
 const ImageSectoin1 = styled.img`
@@ -252,6 +459,16 @@ const ImageSectoin1 = styled.img`
   width:416px;
   height: 240px;
   border-radius: 20px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    height: auto;
+  }
 `;
 
 const ImageSectoin2 = styled.img`
@@ -262,6 +479,16 @@ const ImageSectoin2 = styled.img`
   width:416px;
   height: 240px;
   border-radius: 20px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    height: auto;
+  }
 `;
 
 const ImageSectoin3 = styled.img`
@@ -272,6 +499,16 @@ const ImageSectoin3 = styled.img`
   width:416px;
   height: 240px;
   border-radius: 20px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    height: auto;
+  }
 `;
 
 const ImageSectionContainer1 = styled.div`
@@ -284,6 +521,19 @@ const ImageSectionContainer1 = styled.div`
   border-radius: 20px;
   line-height:250%;
   gap:20px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+    line-height: 200%;
+    gap: 15px;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    line-height: 180%;
+    gap: 10px;
+  }
 `;
 
 const ImageSectionContainer2 = styled.div`
@@ -296,6 +546,19 @@ const ImageSectionContainer2 = styled.div`
   border-radius: 20px;
   line-height:250%;
   gap:20px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+    line-height: 200%;
+    gap: 15px;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    line-height: 180%;
+    gap: 10px;
+  }
 `;
 
 const ImageSectionContainer3 = styled.div`
@@ -308,6 +571,19 @@ const ImageSectionContainer3 = styled.div`
   border-radius: 20px;
   line-height:250%;
   gap:20px;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    height: auto;
+    line-height: 200%;
+    gap: 15px;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    line-height: 180%;
+    gap: 10px;
+  }
 `;
 
 //
@@ -317,31 +593,91 @@ const ImageSectionContainer3 = styled.div`
 const Hyundai = styled.img`
   width: 82px;
   height: 54px;
+
+  @media (max-width: 900px) {
+    width: 70px;
+    height: 46px;
+  }
+
+  @media (max-width: 600px) {
+    width: 60px;
+    height: 40px;
+  }
 `;
 
 const Ford = styled.img`
   width: 115px;
   height: 44px;
+
+  @media (max-width: 900px) {
+    width: 100px;
+    height: 38px;
+  }
+
+  @media (max-width: 600px) {
+    width: 80px;
+    height: 32px;
+  }
 `;
 
 const Mers = styled.img`
   width: 55px;
   height: 54px;
+
+  @media (max-width: 900px) {
+    width: 45px;
+    height: 44px;
+  }
+
+  @media (max-width: 600px) {
+    width: 40px;
+    height: 38px;
+  }
 `;
 
 const Jeep = styled.img`
   width: 109px;
   height: 44px;
+
+  @media (max-width: 900px) {
+    width: 90px;
+    height: 36px;
+  }
+
+  @media (max-width: 600px) {
+    width: 75px;
+    height: 30px;
+  }
 `;
 
 const Bmw = styled.img`
   width: 55px;
   height: 54px;
+
+  @media (max-width: 900px) {
+    width: 45px;
+    height: 44px;
+  }
+
+  @media (max-width: 600px) {
+    width: 40px;
+    height: 38px;
+  }
 `;
 
 const Audi = styled.img`
   width: 124px;
   height: 42px;
+
+  @media (max-width: 900px) {
+    width: 105px;
+    height: 36px;
+  }
+
+  @media (max-width: 600px) {
+    width: 85px;
+    height: 30px;
+  }
 `;
 
 export default function Contact() {
